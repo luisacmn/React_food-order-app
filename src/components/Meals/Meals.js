a6005702cfb1e18a5bcacee8b618975d691b4256
luisacmn <luisanovaes.arq@gmail.com>
2023-04-29 14:44:34 -0300

    Added meals section

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import MealsSumary from './MealsSumary'
+import AvailableMeals from './AvailableMeals'
+
+function Meals() {
+  return (
+    <>
+      <MealsSumary />
+      <AvailableMeals />  
+    </>
+  )
+}
+
+export default Meals
