fbd36c88c65e0c69d10553ca875b536214274b2a
luisacmn <luisanovaes.arq@gmail.com>
2023-04-30 09:12:56 -0300

    Added cart reducer

--- src/context/CartProvider.js
+++ src/context/CartProvider.js
@@ -1,13 +1,38 @@
+import { useReducer } from 'react'
 import CartContext from './cart-context'
 
-const CartProvider = props => {
-    const addItemFromCartHandler = (item) => {}
+const defaultCartState = {
+    items: [],
+    totalAmount: 0
+}
+
+const cartReducer = (state, action) => {
+    if (action.type === 'ADD_ITEM') {
+        const updatedItems = state.items.concat(action.item)
+        const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        }
+    }
     
-    const removeItemFromCartHandler = (id) => {}
+    return defaultCartState
+} 
+
+const CartProvider = props => {
+    const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState)
+
+    const addItemFromCartHandler = (item) => {
+        dispatchCartAction({type: 'ADD_ITEM', item: item})
+    }
+    
+    const removeItemFromCartHandler = (id) => {
+        dispatchCartAction({type: 'REMOVE_ITEM', id: id})
+    }
 
     const cartContext = {
-        items: [],
-        totalAmount: 0,
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
         addItem: addItemFromCartHandler,
         removeItem:removeItemFromCartHandler
     }
